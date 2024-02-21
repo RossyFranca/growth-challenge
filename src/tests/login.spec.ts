8e421019866ea3df56664e950bd68bd0c7beed08
RossyFranca <rossyfranca@gmail.com>
2024-02-20 22:27:18 -0300

    logout complete

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -15,6 +15,21 @@ test.describe('Login Tests', ()=>{
         await page.getByPlaceholder('Senha').fill('Senha.123');
         await page.getByRole('button', { name: 'Entrar' }).click();
 
+        expect(await page.title()).toBe('materiais - Growth Station');
+        expect(page.url()).toBe('https://client.stg.growthstation.app/materials');
+    });
+
+    test.only('logout', async ({page})=>{
+        await page.getByPlaceholder('E-mail').fill('usuariodaempresa3333@gmail.com');
+        await page.getByPlaceholder('Senha').fill('Senha.123');
+        await page.getByRole('button', { name: 'Entrar' }).click();
+
+        
+
+        await page.getByTitle('Usuario').click();
+        await page.locator('[href="/logout"]').click();
+
+        expect(page.url()).toBe('https://client.stg.growthstation.app/login');
 
     });
 
