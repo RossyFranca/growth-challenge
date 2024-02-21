5618d3a515fa6481f2a4373cb5aba4a8c2cf8d54
RossyFranca <rossyfranca@gmail.com>
2024-02-20 23:43:03 -0300

    wrong credentials

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -31,6 +31,28 @@ test.describe('Login Tests', ()=>{
         expect(page.url()).toBe('https://client.stg.growthstation.app/login');
 
     });
+    test('wrong password', async ({page})=>{
+
+        await page.getByPlaceholder('E-mail').fill('usuariodaempresa3333@gmail.com');
+        await page.getByPlaceholder('Senha').fill('Senha.ERRADA');
+        await page.getByRole('button', { name: 'Entrar' }).click();
+
+        const message_error = await page.$eval('.text-left.font-medium.text-xs.text-red-500.w-full', (element) => element.textContent);
+
+        expect(message_error).toBe("*E-mail ou senha inválidos");
+    });
+
+    test('wrong user', async ({page})=>{
+
+        await page.getByPlaceholder('E-mail').fill('invalid_user@gmail.com');
+        await page.getByPlaceholder('Senha').fill('Senha.123');
+        await page.getByRole('button', { name: 'Entrar' }).click();
+
+        const message_error = await page.$eval('.text-left.font-medium.text-xs.text-red-500.w-full', (element) => element.textContent);
+
+        expect(message_error).toBe("*E-mail ou senha inválidos");
+    });
+
 
 
 });
